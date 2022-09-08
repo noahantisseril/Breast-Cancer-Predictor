0365ad88919f51ebb7dd4f311d9e90a2ad9417b8
Dhemath Reddy <65739500+dhem04@users.noreply.github.com>
2022-09-08 03:08:38 -0500

    Update Altering_Data.R

--- Altering_Data.R
+++ Altering_Data.R
@@ -15,3 +15,19 @@ for(i in 1:length(json_data$resources$datahub$type)){
     print(data)
   }
 }
+
+for(i in 1:286){
+  if(data[i,6]==3){
+    data[i,6]="Malignant"
+  }
+  if(data[i,6]==2){
+    data[i,6]="Benign"
+  }
+  if(data[i,6]==1){
+    data[i,6]="Normal"
+  }
+  if(data[i,5]!='yes' && data[i,5]!='no'){
+    data<-data[-i,]
+  }
+}
+write.table(data, file="Data.csv", sep="\t", row.names=FALSE)
