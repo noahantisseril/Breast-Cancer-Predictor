cd9cd5a93cfa9529796d7d7c93f20296bf8fe8d4
Dhemath Reddy <65739500+dhem04@users.noreply.github.com>
2022-09-08 23:23:21 -0500

    Update Altering_Data.R

--- Altering_Data.R
+++ Altering_Data.R
@@ -15,19 +15,46 @@ for(i in 1:length(json_data$resources$datahub$type)){
     print(data)
   }
 }
-
+#Removing empty data points
 for(i in 1:286){
-  if(data[i,6]==3){
-    data[i,6]="Malignant"
-  }
-  if(data[i,6]==2){
-    data[i,6]="Benign"
-  }
-  if(data[i,6]==1){
-    data[i,6]="Normal"
-  }
   if(data[i,5]!='yes' && data[i,5]!='no'){
     data<-data[-i,]
   }
 }
-write.table(data, file="Data.csv", sep="\t", row.names=FALSE)
+#Changing Strings to numbers
+for(i in 1:284){
+  if(data[i,2]=="premeno"){
+    data[i,2]=0
+  }
+  if(data[i,2]=="ge40"){
+    data[i,2]=1
+  }
+  if(data[i,2]=="lt40"){
+    data[i,2]=2
+  }
+  if(data[i,5]=="yes"){
+    data[i,5]=0
+  }
+  if(data[i,5]=="no"){
+    data[i,5]=1
+  }
+  if(data[i,7]=="right"){
+    data[i,7]=0
+  }
+  if(data[i,7]=="left"){
+    data[i,7]=1
+  }
+  if(data[i,9]=="yes"){
+    data[i,9]=0
+  }
+  if(data[i,9]=="no"){
+    data[i,9]=1
+  }
+  if(data[i,10]=="recurrence-events"){
+    data[i,10]=0
+  }
+  if(data[i,10]=="no-recurrence-events"){
+    data[i,10]=1
+  }
+}
+write.table(data, file="Data.csv", sep=",", row.names=FALSE)
